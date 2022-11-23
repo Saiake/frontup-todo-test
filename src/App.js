99230853076c456193b085fb9cb65aef88ac937d
Saiake <sergey.odinokih@yandex.ru>
2022-11-24 00:00:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import ToDoForm from "./components/ToDoForm/ToDoForm"
+import firebase from "./Firebase"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const [toDo, setToDo] = useState([])
+
+    const [addOrEdit, setAddOEdit] = useState(true)
+
+    const takeToDo = () => {
+        firebase
+    }
+
+    return (
+        <div className="App">
+            <ToDoForm addOrEdit={addOrEdit}/>
+        </div>
+    );
+}
+  
+  export default App;
+  
