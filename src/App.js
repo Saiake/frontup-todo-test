85889b1722b61c23e184d6229f952cc5876a0f7b
Saiake <sergey.odinokih@yandex.ru>
2022-11-24 23:28:59 +0500

    beta project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ToDoForm from "./components/ToDoForm/ToDoForm"
-import firebase from "./Firebase"
+import ToDos from "./components/ToDos/ToDos"
+import { getTodosDB } from "./Firebase"
 
 function App() {
 
     const [toDo, setToDo] = useState([])
+    useEffect(()=> {
+        getTodosDB().then(doc => {
+            setToDo(doc)
+        })
+    }, [])
 
     const [addOrEdit, setAddOEdit] = useState(true)
 
     const takeToDo = () => {
-        firebase
+
     }
 
     return (
         <div className="App">
             <ToDoForm addOrEdit={addOrEdit}/>
+            <ToDos toDo={toDo}/>
         </div>
     );
 }
